4d79e8b9ecefe2ffa72d6dd27022e7ba6add0a6b
Robert Bandstra <rob.bandstra@gmail.com>
2021-01-22 20:18:53 -0600

    constructed buildings and displayed the properties of the buildings

---
+++ UrbanPlanner/Building.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrbanPlanner
+{
+    class Building
+    {
+        //Fields
+        private string _designer;
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+
+        //Properties
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume { get { return Width * Depth * (3 * Stories); } }
+        
+        //Constructor
+        public Building(string address)
+        {
+            _address = address;
+            _designer = "Rob Bandstra";
+        }
+
+        //Methods
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string purchaser)
+        {
+            _owner = purchaser;
+        }
+
+        public void Display()
+        {
+            Console.Write(
+                    @$"{_address}
+Designed by {_designer}
+Constructed on {_dateConstructed}
+Owned by {_owner}
+{Volume} cubic meters of space
+
+");
+        }
+
+    }
+}
---
+++ UrbanPlanner/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace UrbanPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building FiveOneTwoEighth = new Building("512 N 8th Ave")
+            {
+                Width = 50.5,
+                Depth = 60.7,
+                Stories = 5,
+            };
+
+            Building SimonMed = new Building("6740 E Camelback Road")
+            {
+                Width = 132.8,
+                Depth = 92.3,
+                Stories = 1
+            };
+
+            Building Upmc = new Building("201 State St")
+            {
+                Width = 220.1,
+                Depth = 189.9,
+                Stories = 12
+            };
+
+            Building LakewoodRanch = new Building("8330 Lakewood Ranch Blvd")
+            {
+                Width = 187.7,
+                Depth = 99.2,
+                Stories = 3
+            };
+
+            FiveOneTwoEighth.Construct();
+            SimonMed.Construct();
+            Upmc.Construct();
+            LakewoodRanch.Construct();
+
+            FiveOneTwoEighth.Purchase("Brent Weeks");
+            SimonMed.Purchase("Patrick Rothfuss");
+            Upmc.Purchase("Brandon Sanderson");
+            LakewoodRanch.Purchase("N. K. Jemisin");
+
+            FiveOneTwoEighth.Display();
+            SimonMed.Display();
+            Upmc.Display();
+            LakewoodRanch.Display();
+        }
+    }
+}
